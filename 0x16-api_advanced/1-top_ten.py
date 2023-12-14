b22b405ecb9e39932dd1f8519be5fc697e119bc8
Purengugi <purity2004ngugi@gmail.com>
2023-12-14 14:06:25 +0000

    update

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,30 +1,12 @@
 #!/usr/bin/python3
 """
-   queries the Reddit API and prints the titles of the
-   first 10 hot posts
+1-main
 """
+import sys
 
-import requests
-
-
-def top_ten(subreddit):
-    """
-    print titles of the first 10 hot posts
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-
-    headers = {'User-Agent': 'My user Agent 1.0'}
-
-    # GET request
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            children = response.json().get('data').get('children')
-            for i in range(10):
-                print(children[i]).get('data').get('title')
-        else:
-            print("None")
-    except Exception:
-        print("None")
+if __name__ == '__main__':
+    top_ten = __import__('1-top_ten').top_ten
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])
